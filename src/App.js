543950225bc3d346ba7350c29857f495975f892f
Gokul <gokulnath0827@gmail.com>
2023-01-22 00:50:30 +0530

    done with responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import { FaArrowUp } from 'react-icons/fa'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { FaArrowUp } from "react-icons/fa";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 function App() {
+  
   return (
-    <div className="App">
-      <Navbar />
-      <main className="main-container" style={{paddingTop: "100vh"}}>
-        <section  className='hero-section' id="hero">Hero section</section>
-        <section id="call-for-paper">Call for Paper</section>
-        <section id="paper-submission">Paper Submission</section>
-        <a className='back-to-top' href="#">
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <main className="main-container" style={{ paddingTop: "100vh" }}>
+          <section className="hero-section" id="hero">
+            Hero section
+          </section>
+          <section id="call-for-paper">Call for Paper</section>
+          <section id="paper-submission">Paper Submission</section>
+        </main>
+        <HashLink className="back-to-top" to="#">
           <FaArrowUp />
-        </a>
-      </main>
-    </div>
+        </HashLink>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,51 +1,84 @@
-import React from 'react'
-import logo from '../../assets/logo.webp'
-import './navbar.css'
-import { HashLink } from 'react-router-hash-link'
-import { useState } from 'react'
-
+import React from "react";
+import logo from "../../assets/logo.webp";
+import "./navbar.css";
+import { HashLink } from "react-router-hash-link";
+import { useState } from "react";
+import { CgMenuRight } from "react-icons/cg";
+import { CgClose } from "react-icons/cg";
 const Navbar = () => {
-    const [colorChange, setColorchange] = useState(false);
+    const [colorChange, setColorchange] = useState(false)
+    const [mobnavOpen, setMobilenavOpen] = useState(false)
     const changeNavbarColor = () => {
         if (window.scrollY >= 80) {
             setColorchange(true);
-        }
-        else {
+        } else {
             setColorchange(false);
         }
     };
-    window.addEventListener('scroll', changeNavbarColor);
-
+    window.addEventListener("scroll", changeNavbarColor);
+    const setNavHeight = () => {
+        const nav = document.querySelector("#navbar")
+        const root = document.querySelector(":root")
+        root.style.setProperty('--navHeight', `${nav.clientHeight}px`)
+    }
+    window.addEventListener('resize', setNavHeight)
     return (
-    <nav className={colorChange ? 'navbar navbar-active' : 'navbar'}>
-        <div className="container">
-            <div className="logo">
-                <img style={{ width: "100%" }} src={logo} alt="logo" />
+        <nav id="navbar" className={colorChange ? "navbar navbar-active" : "navbar"}>
+            <div className="container">
+                <div className="logo">
+                    <img style={{ width: "100%" }} src={logo} alt="logo" />
+                </div>
+                <div className="nav-elements">
+                    <ul className={mobnavOpen ? "active" : ""}>
+                        <li>
+                            <HashLink
+                                to="#"
+                                onClick={() => setMobilenavOpen(!mobnavOpen)}
+                                >
+                                Home
+                            </HashLink>
+                        </li>
+                        <li>
+                            <HashLink
+                                to="#call-for-paper"
+                                onClick={() => setMobilenavOpen(!mobnavOpen)}
+                                >Call For Paper</HashLink>
+                        </li>
+                        <li>
+                            <HashLink
+                                to="#paper-submission"
+                                onClick={() => setMobilenavOpen(!mobnavOpen)}
+                                >Paper Submission</HashLink>
+                        </li>
+                        <li>
+                            <HashLink
+                                to="#about"
+                                onClick={() => setMobilenavOpen(!mobnavOpen)}
+                            >About</HashLink>
+                        </li>
+                        <li>
+                            <HashLink
+                                to="#committee"
+                                onClick={() => setMobilenavOpen(!mobnavOpen)}
+                            >Committee</HashLink>
+                        </li>
+                        <li>
+                            <HashLink
+                                to="#contact"
+                                onClick={() => setMobilenavOpen(!mobnavOpen)}
+                            >Contact</HashLink>
+                        </li>
+                    </ul>
+                </div>
+                <div
+                    id="mobile"
+                    onClick={() => setMobilenavOpen(!mobnavOpen)}>
+                    {mobnavOpen ? <CgClose /> : <CgMenuRight />}
+                </div>
             </div>
-            <div className="nav-elements">
-                <ul>
-                    <li>
-                        <a href="/#">Home</a>
-                    </li>
-                    <li>
-                        <a href="/#call-for-paper">Call For Paper</a>
-                    </li>
-                    <li>
-                        <a href="/#paper-submission">Paper submission</a>
-                    </li>
-                    <li>
-                        <a href="">About</a></li>
-                    <li>
-                        <a href="">Committee</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    )
-}
+        </nav>
+        
+    );
+};
 
-export default Navbar
+export default Navbar;
